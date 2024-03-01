3ba867997dc5e0e45d712b7ba1b36816c0443559
VibesAR <canchegamer189@gmail.com>
2024-03-01 01:03:37 -0600

    Primer Commit

---
+++ Ejercicio 4 Parcial/Ejercicio 4 Parcial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 101);
+
+        bool adivinado = false;
+        int numeroIntroducido;
+
+        while (!adivinado)
+        {
+            Console.WriteLine("Escriba un número entre 1 y 100: ");
+            numeroIntroducido = int.Parse(Console.ReadLine());
+
+            if (numeroIntroducido > numeroSecreto)
+            {
+                Console.WriteLine("El número secreto es menor que {0}", numeroIntroducido);
+            }
+            else if (numeroIntroducido < numeroSecreto)
+            {
+                Console.WriteLine("El número secreto es mayor que {0}", numeroIntroducido);
+            }
+            else
+            {
+                adivinado = true;
+                Console.WriteLine("¡Chilero! Has adivinado el número secreto: {0}", numeroSecreto);
+            }
+        }
+    }
+}
